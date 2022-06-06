7e29392ac1a6eef4343c86f6d4d4dadea10741b8
Jose Manuel Lezcano Fresno <jmlezcano@atsistemas.com>
2022-06-06 16:50:34 +0200

    first test it (error)

---
+++ src/main/java/org/hero/controller/HeroGetController.java
@@ -0,0 +1,12 @@
+package org.hero.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public final class HeroGetController {
+
+
+  //ResponseEntity
+  //ResponseEntity
+}
---
+++ src/test/java/org/hero/controller/HeroGetControllerShould.java
@@ -0,0 +1,13 @@
+package org.hero.controller;
+
+import org.hero.application.HeroApplicationTestCase;
+import org.junit.jupiter.api.Test;
+
+class HeroGetControllerShould extends HeroApplicationTestCase {
+
+
+  @Test
+  void obtain_all_super_hero() throws Exception {
+    assertRequest("GET", "heroes", 201);
+  }
+}
